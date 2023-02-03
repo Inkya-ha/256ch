bfbaee74e75d1de699ad1bdb0a9bc069b6ecc24e
Inkya-ha <admin@inkya-ha.com>
2023-02-03 22:47:24 +0900

    利用規約の追加

---
+++ Licence.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php include_once __DIR__ . "/static/head/head.php"; ?>
+    <title>利用規約｜256channel</title>
+</head>
+<body>
+    <?php include_once __DIR__ . "/static/header/header.php"; ?>
+
+    <main>
+    <main>
+        <div class="box6">
+            <p class="fs-1">利用規約</p>
+            <hr style="color: black;">
+            <div class="privacy-mg">
+                <p class="fs-5 scroll-up">
+                    ・第一条 (本利用規約について)<br><br>
+                </p>    
+                <p class="fs-6 scroll-up">
+                    本利用規約は256ch「以降、当社といいます」が提供するサービス「以降、当社といいます」が提供するサービス「以降、当サービスといいます」をご利用していただくにあたって適用されるものです。<br>
+                    当社が提供するサービス全てに本規約が適用されます。<br><br>
+                </p>
+                <p class="fs-5 scroll-up">
+                    第二条 (本規約の適用開始時間)<br>
+                </p>
+                <p class="fs-6 scroll-up">
+                    本規約は2023年02月01日00時00分「日本時間 UTC+0900」より適用されます。<br><br>
+                </p>
+                <p class="fs-5 scroll-up">
+                    第三条 (本利用規約の変更等)<br>
+                </p>
+                <p class="fs-6 scroll-up">
+                    当社の管理者は、本規約を何時でも追記・変更等できるものとします。<br>
+                    管理者が利用規約を変更した際には改めて改変後の規約が適用されます<br><br>
+                </p>
+                <p class="fs-5 scroll-up">
+                    第四条 (当社の目的)<br>
+                </p>
+                <p class="fs-6 scroll-up">
+                    当社はプログラミングや機械学習・セキュリティ・ネットワークなどの主にネット上で活動している方向けへのコミュニティです。<br><br>
+                </p>
+                <p class="fs-5 scroll-up">
+                    第五条 (サービスの利用により発生する責任について)<br>
+                </p>
+                <p class="fs-6 scroll-up">
+                    当社が提供するサービスで商用利用をすることを可能とします。<br>
+                    万が一セキュリティ上に問題が存在した場合や何らかの問題でサービスが停止した際にはお客様のご利益の保証・損害賠償請求に応じることはできません。<br>
+                    当社のサービスに負荷をかける行為・利益に損害が出る行為・脆弱性等使用してサーバーを悪用する行為等損失が発生した場合は当社がお客様に対して発生した損失相当額分の損害賠償請求を行うことができるものとします。<br><br>
+                </p>
+                <p class="fs-5 scroll-up">
+                    第六条 (他企業様からの賠償請求について)<br>
+                </p>
+                <p class="fs-6 scroll-up">
+                    当社が契約しているサーバーの提供事業者様・リバースプロキシの提供事業者様からも損害賠償請求を行うことができます。<br>
+                    その際に確定した損害賠償金は当社が負担・保証することはできません。<br><br>
+                </p>
+                <p class="fs-5 scroll-up">
+                    第七条 (準拠法・裁判管轄)<br>
+                </p>
+                <p class="fs-6 scroll-up">
+                    本規約の解釈にあたっては、日本法を準拠法とします。<br>
+                    本サービスに関して紛争が生じた場合には、当社の本所在地を管轄する裁判所を専属的合意管轄とします。<br><br>
+                </p>
+            </div>
+        </div>
+    </main>
+    
+    <?php include_once __DIR__ . "/static/footer/footer.php"; ?>
+</body>
+</html>
