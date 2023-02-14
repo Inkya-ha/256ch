2b5187e889953fed76faf62fb6b03d9b62a7b2dc
Inkya-ha <admin@inkya-ha.com>
2023-02-14 13:43:01 +0900

    脆弱性テスト失敗したわ.com

--- class/User.php
+++ class/User.php
@@ -5,59 +5,40 @@
 
 -->
 
-<?php
-require_once __DIR__ . "/../connect_db.php";
-
-class User
-{
-    protected $uuid;
-
-    function __construct($dataType, $dataBody)
-    {
-        if (!in_array($dataType, ["uuid", "email"])) {
-            throw new InvalidArgumentException("Invalid data type");
-        }
-
-        if (!$dataBody) {
-            throw new InvalidArgumentException("Data body must not be empty");
-        }
-
-        if ($dataType === "uuid") {
-            $this->uuid = $dataBody;
-        } else if ($dataType === "email") {
-            $pdo = connect();
-            $sql = "SELECT * FROM userdata WHERE EMAIL = :email";
-            $statement = $pdo->prepare($sql);
-            $statement->bindValue(':email', $dataBody, PDO::PARAM_STR);
-            $statement->execute();
-            $row = $statement->fetch(PDO::FETCH_ASSOC);
-
-            if (!$row) {
-                throw new InvalidArgumentException("User not found");
-            }
-
-            $this->uuid = $row['UUID'];
-        }
-    }
-
-    function getUuid()
-    {
-        return $this->uuid;
-    }
-
-    function getEmail()
-    {
-        $pdo = connect();
-        $sql = "SELECT * FROM userdata WHERE UUID = :uuid";
-        $statement = $pdo->prepare($sql);
-        $statement->bindValue(':uuid', $this->uuid, PDO::PARAM_STR);
-        $statement->execute();
-        $row = $statement->fetch(PDO::FETCH_ASSOC);
-
-        if (!$row) {
-            throw new InvalidArgumentException("User not found");
-        }
-
-        return htmlentities($row['EMAIL'], ENT_QUOTES, "UTF-8");
-    }
+<?php 
+require_once __DIR__ . "/../connect_db.php";//pdo接続用 
+class User{
+	protected $uuid;
+	
+	//コンストラクタ。ユニークキーの種類、ユニークキーの値を受け取り、個人を特定する。
+	function __construct($dataType,$dataBody){
+		//dataTypeはemail,uuidから指定。
+		if($dataType == "uuid"){
+			$this->uuid = $dataBody;
+		}else if($dataType == "email"){
+			$pdo = connect();
+			$sql = "SELECT * FROM userdata WHERE EMAIL = :email";
+			$statement = $pdo->prepare($sql);
+			$statement->bindValue(':email',$dataBody,PDO::PARAM_STR);
+			$statement->execute();
+			$row = $statement->fetch(PDO::FETCH_ASSOC);
+			$this->uuid = $row['UUID'];
+			//echo ($this->uuid);
+		}
+	}
+	//メソッドの定義
+	//uuidを取得
+	function uuid(){
+		return $this->uuid;
+	}
+	//メールアドレスを取得
+	function email(){
+		$pdo = connect();
+		$sql = "SELECT * FROM userdata WHERE UUID = :uuid";
+		$statement = $pdo->prepare($sql);
+		$statement->bindValue(':uuid',$this->uuid,PDO::PARAM_STR);
+		$statement->execute();
+		$row = $statement->fetch(PDO::FETCH_ASSOC);
+		return $row['EMAIL'];
+	}
 }
